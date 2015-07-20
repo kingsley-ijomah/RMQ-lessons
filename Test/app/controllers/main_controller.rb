c21a607c91b12d5d1bf52576aa4df2b6043adfaa
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-20 20:18:28 +0100

    Strip controller down to empty viewDidLoad method

--- Test/app/controllers/main_controller.rb
+++ Test/app/controllers/main_controller.rb
@@ -2,56 +2,6 @@ class MainController < UIViewController
 
   def viewDidLoad
     super
-
-    # Sets a top of 0 to be below the navigation control, it's best not to do this
-    # self.edgesForExtendedLayout = UIRectEdgeNone
-
-    rmq.stylesheet = MainStylesheet
-    init_nav
-    rmq(self.view).apply_style :root_view
-
-    # Create your UIViews here 
-    @hello_world_label = rmq.append!(UILabel, :hello_world)
-
-    rmq.append(UIButton, :send_button).on(:tap) { puts 'tapped!' }
-  end
-
-  def init_nav
-    self.title = 'Title Here'
-
-    self.navigationItem.tap do |nav|
-      nav.leftBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAction,
-                                                                           target: self, action: :nav_left_button)
-      nav.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemRefresh,
-                                                                           target: self, action: :nav_right_button)
-    end
-  end
-
-  def nav_left_button
-    puts 'Left button'
-  end
-
-  def nav_right_button
-    puts 'Right button'
-  end
-
-  # Remove these if you are only supporting portrait
-  def supportedInterfaceOrientations
-    UIInterfaceOrientationMaskAll
-  end
-  def willAnimateRotationToInterfaceOrientation(orientation, duration: duration)
-    # Called before rotation
-    rmq.all.reapply_styles
-  end
-  def viewWillLayoutSubviews
-    # Called anytime the frame changes, including rotation, and when the in-call status bar shows or hides
-    #
-    # If you need to reapply styles during rotation, do it here instead
-    # of willAnimateRotationToInterfaceOrientation, however make sure your styles only apply the layout when
-    # called multiple times
-  end
-  def didRotateFromInterfaceOrientation(from_interface_orientation)
-    # Called after rotation
   end
 end
 

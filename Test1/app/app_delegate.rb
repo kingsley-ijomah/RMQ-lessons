210a6080bb12dfa4baa76444fb4019765495d853
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-23 16:45:33 +0100

    Testing apps windows

--- Test1/app/app_delegate.rb
+++ Test1/app/app_delegate.rb
@@ -1,3 +1,6 @@
 class AppDelegate
-
+  def application(app, didFinishLaunchingWithOptions: launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    true
+  end
 end

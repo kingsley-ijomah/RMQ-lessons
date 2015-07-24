a6a1a021ef8091c5b3517d8972daa8560ab02118
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-24 14:58:34 +0100

    Testing CGSize and CGPoint

--- Test1/app/app_delegate.rb
+++ Test1/app/app_delegate.rb
@@ -1,6 +1,10 @@
 class AppDelegate
   def application(app, didFinishLaunchingWithOptions: launchOptions)
-    puts CGRectGetHeight(UIScreen.mainScreen.bounds)
+    @rect = UIScreen.mainScreen.bounds
+    puts @rect.size
+    puts @rect.origin
+    puts @rect.size.width
+    puts @rect.origin.y 
     true
   end
 end

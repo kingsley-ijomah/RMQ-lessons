4e6115ded2f636e093d6fb61a14f20a451a95056
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-25 07:56:46 +0100

    Add left and right bar button

--- Test1/app/controllers/main_controller.rb
+++ Test1/app/controllers/main_controller.rb
@@ -2,5 +2,22 @@ class MainController < UIViewController
   def viewDidLoad
     super
     self.view.backgroundColor = UIColor.blueColor
+    self.title = 'Demo'
+    self.navigationItem.tap do |nav|
+      nav.leftBarButtonItem = create_bar_button(UIBarButtonSystemItemSearch, :left_bar_btn)
+      nav.rightBarButtonItem = create_bar_button(UIBarButtonSystemItemEdit, :right_bar_btn)
+    end
+  end
+
+  def create_bar_button(type, action)
+     UIBarButtonItem.alloc.initWithBarButtonSystemItem(type, target: self, action: action)
+  end
+
+  def left_bar_btn
+    puts 'left bar button'
+  end
+
+  def right_bar_btn
+    puts 'right bar button'
   end
 end

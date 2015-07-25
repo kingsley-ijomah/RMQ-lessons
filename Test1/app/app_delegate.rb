e43b754d24786ede00f8a13bd805ee95d0c3764d
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-25 06:32:34 +0100

    Add root view controller

--- Test1/app/app_delegate.rb
+++ Test1/app/app_delegate.rb
@@ -2,6 +2,7 @@ class AppDelegate
   def application(app, didFinishLaunchingWithOptions: launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
+    @window.rootViewController = MainController.new
     true
   end
 end

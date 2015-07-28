98b04226d267fe9e00041c1b97c004e3b0fce6af
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-28 18:41:01 +0100

    Remove UIAlert

--- UIAlert/app/app_delegate.rb
+++
@@ -1,8 +0,0 @@
-class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
-    @window.rootViewController = MainController.new
-    true
-  end
-end

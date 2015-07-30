0d1f89cd4d27ff9acae50abd0886989c71ef0929
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-30 13:24:58 +0100

    Add app delegate skeleton

--- Navigation/app/app_delegate.rb
+++ Navigation/app/app_delegate.rb
@@ -1,8 +1,3 @@
 class AppDelegate
-  def application(application, didFinishLaunchingWithOptions: launchOption)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
-    true
-  end
-end
 
+end

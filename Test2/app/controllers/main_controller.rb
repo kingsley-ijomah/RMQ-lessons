8c51af4ec0d9c94b28fe1954d54ba9e4e19086ac
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-02 21:49:45 +0100

    Add bar buttons

--- Test2/app/controllers/main_controller.rb
+++ Test2/app/controllers/main_controller.rb
@@ -2,13 +2,20 @@ class MainController < UIViewController
   def viewDidLoad
     super
     self.title = 'Main...'
+
     self.navigationItem.tap do |nav|
       nav.leftBarButtonItem = create_bar_button(UIBarButtonSystemItemSave, :left_btn)
       nav.rightBarButtonItem = create_bar_button(UIBarButtonSystemItemAdd, :right_btn)
     end
   end
 
+  def create_bar_button(type, action)
+    UIBarButtonItem.alloc.initWithBarButtonSystemItem(type, target: self, action: action)  
+  end
+
   def left_btn
-  
+  end
+
+  def right_btn
   end
 end

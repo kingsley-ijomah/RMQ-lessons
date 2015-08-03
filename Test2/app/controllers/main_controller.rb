93f3872d44dcc9556432d009d1e9290b1f9d88be
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-03 17:33:42 +0100

    update test

--- Test2/app/controllers/main_controller.rb
+++ Test2/app/controllers/main_controller.rb
@@ -1,10 +1,9 @@
 class MainController < UIViewController
   def viewDidLoad
     super
-    self.title = 'Main...'
-
     self.navigationItem.tap do |nav|
-      nav.leftBarButtonItem = create_bar_button(UIBarButtonSystemItemSave, :left_btn)
+      nav.title = 'Hello'
+      nav.leftBarButtonItem = editButtonItem
       nav.rightBarButtonItem = create_bar_button(UIBarButtonSystemItemAdd, :right_btn)
     end
   end

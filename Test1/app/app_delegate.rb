21421bd68d5b6c05d34da794f2eacb84d71c14f8
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-04 20:59:43 +0100

    remove test1

--- Test1/app/app_delegate.rb
+++
@@ -1,8 +0,0 @@
-class AppDelegate
-  def application(app, didFinishLaunchingWithOptions: launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(MainController.new)
-    true
-  end
-end
--- Test1/spec/main_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Application 'Test1'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
-  end
-end

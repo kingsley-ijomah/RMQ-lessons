54d344c8f25e359fcf30725a52e598cef839b7df
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-06 18:00:05 +0100

    Add tabBarController

--- Navigation/app/app_delegate.rb
+++ Navigation/app/app_delegate.rb
@@ -2,7 +2,12 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions: launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(MainController.alloc.init)
-    true
+
+    tabBar = UITabBarController.alloc.init
+    tabBar.viewControllers = [
+      UINavigationController.alloc.initWithRootViewController(MainController.alloc.init),
+      UINavigationController.alloc.initWithRootViewController(SecondaryController.alloc.init)]
+ 
+    @window.rootViewController = tabBar 
   end
 end

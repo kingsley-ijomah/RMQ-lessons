430a5b6fc72c8ebbe57a18bba731ea600f69bdc5
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-06 18:00:38 +0100

    Add UITabBarItem

--- Navigation/app/controllers/secondary_controller.rb
+++ Navigation/app/controllers/secondary_controller.rb
@@ -1,4 +1,11 @@
 class SecondaryController < UIViewController
+  def init
+    if super
+      self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag:1)
+    end
+    self
+  end
+
   def viewDidLoad
     super
     self.view.backgroundColor = UIColor.redColor

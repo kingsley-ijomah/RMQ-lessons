d09a72cdb190b329a36fe46261eaed33c35c377a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-18 09:21:59 +0100

    Adding tabBarItem

--- simple_table_demo/app/controllers/second_table_controller.rb
+++ simple_table_demo/app/controllers/second_table_controller.rb
@@ -1,3 +1,9 @@
 class SecondTableController < UIViewController
+  def init
+    if super
+      self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemMore, tag:1)
+    end
+    self
+  end
 
 end

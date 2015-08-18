44b449d771d9ea143ea27f806e41d549899ecc9f
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-18 10:44:02 +0100

    Adding cellForRowAtIndexPath

--- simple_table_demo/app/controllers/first_table_controller.rb
+++ simple_table_demo/app/controllers/first_table_controller.rb
@@ -1,8 +1,23 @@
 class FirstTableController < UITableViewController
   def initWithStyle(style)
     if super
-      self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag:1)
+      self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemContacts, tag:1)
     end
     self
+  end
+
+  def viewDidLoad
+    super
+    @data = ('A'..'Z').to_a
+  end
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    @data.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:nil).tap do |c|
+      c.textLabel.text = @data[indexPath.row]
+    end
   end
 end

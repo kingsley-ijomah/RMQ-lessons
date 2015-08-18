70d8a5ba87fee5751e514dfd2ca68448f47767a5
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-08-18 09:21:43 +0100

    Adding tabBarItem

--- simple_table_demo/app/controllers/first_table_controller.rb
+++ simple_table_demo/app/controllers/first_table_controller.rb
@@ -1,3 +1,8 @@
 class FirstTableController < UITableViewController
-
+  def initWithStyle(style)
+    if super
+      self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag:1)
+    end
+    self
+  end
 end

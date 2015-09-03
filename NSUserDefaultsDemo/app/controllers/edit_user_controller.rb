47a2e3d11d3f576a6535915084460f68d5b2725e
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-03 06:51:21 +0100

    Add form elements

--- NSUserDefaultsDemo/app/controllers/edit_user_controller.rb
+++ NSUserDefaultsDemo/app/controllers/edit_user_controller.rb
@@ -2,6 +2,38 @@ class EditUserController < UIViewController
   def viewDidLoad
     super
     self.view.BackgroundColor = UIColor.whiteColor
-    self.view.title = "Edit User"
+    self.title = "Edit User"
+
+    id_field = UITextField.alloc.init.tap do |f|
+      f.frame = [[20, 100], [335, 35]]
+      f.borderStyle = UITextBorderStyleRoundedRect
+      f.placeholder = "Enter ID"
+      f.textAlignment = UITextAlignmentCenter
+    end
+    self.view.addSubview(id_field)
+
+    name_field  = UITextField.alloc.init.tap do |f|
+      f.frame = [[20, 150], [335, 35]]
+      f.borderStyle = UITextBorderStyleRoundedRect
+      f.placeholder = "Enter Full Name"
+      f.textAlignment = UITextAlignmentCenter
+    end
+    self.view.addSubview(name_field)
+
+    email_field  = UITextField.alloc.init.tap do |f|
+      f.frame = [[20, 200], [335, 35]]
+      f.borderStyle = UITextBorderStyleRoundedRect
+      f.placeholder = "Enter Email Address"
+      f.textAlignment = UITextAlignmentCenter
+    end
+    self.view.addSubview(email_field)
+
+    submit_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
+      b.frame = [[20, 250], [335, 35]]
+      b.setTitle("Submit Form", forState:UIControlStateNormal)
+      b.addTarget(self, action:"buttonPressed", forControlEvents:UIControlEventTouchDown)
+    end
+    self.view.addSubview(submit_button)
   end
+
 end

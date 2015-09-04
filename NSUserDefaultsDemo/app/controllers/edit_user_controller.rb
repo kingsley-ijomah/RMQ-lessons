47d9554d2f061cfb778be353265ec4eea66fb444
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-04 17:50:10 +0100

    Ability to save user

--- NSUserDefaultsDemo/app/controllers/edit_user_controller.rb
+++ NSUserDefaultsDemo/app/controllers/edit_user_controller.rb
@@ -10,7 +10,7 @@ class EditUserController < UIViewController
       f.placeholder = "Enter ID"
       f.textAlignment = UITextAlignmentCenter
     end
-    self.view.addSubview(id_field)
+    self.view.addSubview(@id_field)
 
     @name_field  = UITextField.alloc.init.tap do |f|
       f.frame = [[20, 150], [335, 35]]
@@ -18,7 +18,7 @@ class EditUserController < UIViewController
       f.placeholder = "Enter Full Name"
       f.textAlignment = UITextAlignmentCenter
     end
-    self.view.addSubview(name_field)
+    self.view.addSubview(@name_field)
 
     @email_field  = UITextField.alloc.init.tap do |f|
       f.frame = [[20, 200], [335, 35]]
@@ -26,7 +26,7 @@ class EditUserController < UIViewController
       f.placeholder = "Enter Email Address"
       f.textAlignment = UITextAlignmentCenter
     end
-    self.view.addSubview(email_field)
+    self.view.addSubview(@email_field)
 
     submit_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
       b.frame = [[20, 250], [335, 35]]
@@ -37,9 +37,8 @@ class EditUserController < UIViewController
   end
 
   def submit
-    puts @id_field.text
-    puts @name_field.text
-    puts @email_fiel.text
+    @user = User.new(id:@id_field.text, name:@name_field.text, email:@email_field.text)
+    User.write(@user)
   end
 
 end
